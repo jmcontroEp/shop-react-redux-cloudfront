66420a1830e18c6b0e3e51a894e4287c1000abf8
Jose Miguel Contro <josemiguel_contro@epam.com>
2023-12-20 12:56:58 -0600

    Modified Import API Path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
 	product: 'https://ho4bntl0vi.execute-api.us-east-2.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://wv4gmwjgvj.execute-api.us-east-2.amazonaws.com/dev/import',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
