66857b477093cc5ff88986ea72aa6f14de3b74d2
Jose Miguel Contro <josemiguel_contro@epam.com>
2024-01-18 10:27:42 -0600

    Finished Task 7

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,60 +1,79 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import axios from 'axios';
 
 type CSVFileImportProps = {
-  url: string;
-  title: string;
+	url: string;
+	title: string;
 };
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
-  const [file, setFile] = React.useState<File>();
+	function encodeToBase64(value: string): string {
+		const encoder = new TextEncoder();
+		const encodedArray = encoder.encode(value);
+		return btoa(String.fromCharCode(...encodedArray));
+	}
 
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files && files.length > 0) {
-      const file = files[0];
-      setFile(file);
-    }
-  };
+	const username = 'jmcontroEp';
+	const password = 'TEST_PASSWORD';
 
-  const removeFile = () => {
-    setFile(undefined);
-  };
+	const concatenatedValue = `${encodeToBase64(username)}:${encodeToBase64(
+		password
+	)}`;
 
-  const uploadFile = async () => {
-    console.log("uploadFile to", url);
+	localStorage.setItem('authorization_token', concatenatedValue);
 
-    // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
-  };
-  return (
-    <Box>
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      {!file ? (
-        <input type="file" onChange={onFileChange} />
-      ) : (
-        <div>
-          <button onClick={removeFile}>Remove file</button>
-          <button onClick={uploadFile}>Upload file</button>
-        </div>
-      )}
-    </Box>
-  );
+	const authorization_token = localStorage.getItem('authorization_token');
+	const [file, setFile] = React.useState<File>();
+
+	const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files;
+		if (files && files.length > 0) {
+			const file = files[0];
+			setFile(file);
+		}
+	};
+
+	const removeFile = () => {
+		setFile(undefined);
+	};
+
+	const uploadFile = async () => {
+		console.log('uploadFile to', url);
+
+		// Get the presigned URL
+		// const response = await axios({
+		//   method: "GET",
+		//   url,
+		//   params: {
+		//     name: encodeURIComponent(file.name),
+		//     Authorization: Basic {authorization_token},
+		//   },
+		// });
+		// console.log("File to upload: ", file.name);
+		// console.log("Uploading to: ", response.data);
+		// const result = await fetch(response.data, {
+		//   method: "PUT",
+		//   body: file,
+		// });
+		// console.log("Result: ", result);
+		// setFile("");
+	};
+
+	return (
+		<Box>
+			<Typography variant='h6' gutterBottom>
+				{title}
+			</Typography>
+			{!file ? (
+				<input type='file' onChange={onFileChange} />
+			) : (
+				<div>
+					<button onClick={removeFile}>Remove file</button>
+					<button onClick={uploadFile}>Upload file</button>
+				</div>
+			)}
+		</Box>
+	);
 }
